d94909442029ad6f4bf1808fed83743286938129
JhonKb <jhonklebson11@gmail.com>
2023-09-30 09:53:26 -0300

    Adicionar arquivos de projeto.

---
+++ EleicoesUni/EleicoesUni/Model/Turma.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EleicoesUni.Model
+{
+    internal class Turma
+    {
+    }
+}
---
+++ EleicoesUni/EleicoesUni/View/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EleicoesUni
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            turma.Items.Add("Turma ADS 1/2");
+            turma.Items.Add("Turma ADS 3/4");
+            turma.Items.Add("Turma Fisio 1");
+            turma.Items.Add("Turma Fisio 2");
+            turma.Items.Add("Turma Direito 5");
+        }
+    }
+}
